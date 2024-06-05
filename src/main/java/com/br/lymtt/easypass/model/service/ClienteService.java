efb83efb30fad7874f794cf0866a6dac69ca4a64
2002Larissa <larissadssantos1515@gmail.com>
2024-06-05 14:43:30 -0300

    Serviço do cliente

--- src/main/java/com/br/lymtt/easypass/model/service/ClienteService.java
+++ src/main/java/com/br/lymtt/easypass/model/service/ClienteService.java
@@ -1,5 +1,56 @@
 package com.br.lymtt.easypass.model.service;
 
+import com.br.lymtt.easypass.model.entities.Cliente;
+import com.br.lymtt.easypass.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClienteService {
-    
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    //CADASTRANDO
+    public Cliente cadastrar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    //LISTANDO
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> listarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    //ATUALIZANDO ESSE MALDITO
+    public Cliente atualizar(Long id, Cliente updatedCliente) {
+        Optional<Cliente> existingCliente = clienteRepository.findById(id);
+        if (existingCliente.isPresent()) {
+            Cliente cliente = existingCliente.get();
+            cliente.setNome(updatedCliente.getNome());
+            cliente.setCpf(updatedCliente.getCpf());
+            cliente.setGenero(updatedCliente.getGenero());
+            cliente.setIdade(updatedCliente.getIdade());
+            cliente.setEmail(updatedCliente.getEmail());
+            cliente.setNumeroTelefone(updatedCliente.getNumeroTelefone());
+            return clienteRepository.save(cliente);
+        } else {
+            return null;
+        }
+    }
+    //DELETANDO ESSE CACETE
+    public boolean deletar(Long id) {
+        if (clienteRepository.existsById(id)) {
+            clienteRepository.deleteById(id);
+            return true;
+        } else {
+            return false; 
+        }
+    }
 }
